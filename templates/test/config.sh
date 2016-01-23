4816e25ba9324ac9e769072898e876070301d40a
Moritz Neikes <mneikes@gmx.de>
2016-01-23 02:00:13 +0100

    Add 'include' command that allows users to include files from the 'include' folder

--- templates/test/config.sh
+++ templates/test/config.sh
@@ -9,6 +9,19 @@ f_before()
 	# This code is executed before files are copied to the destination.
 	dest="`pwd`/.test"
 
+	f_include extrafile.txt
+	f_include extra\ file\ with\ spaces.txt
+	f_include extra\ file\ with\ spaces.txt customname.txt
+	f_include extra\ file\ with\ spaces.txt nested/customname.txt
+	f_include extra\ file\ with\ spaces.txt nested/custom\ name\ with\ spaces.txt
+	f_include extra\ file\ with\ spaces.txt nested\ folder\ with\ spaces/custom\ name\ with\ spaces.txt
+
+
+	if [[ ! `f_include non-existing-file.txt` ]]; then
+		echo "error: expected command to fail:"
+		echo "f_include non-existing-file.txt"
+		exit 1
+	fi
 
 	# Note that this function must not be completely empty,
 	# hence the `return` statement.
@@ -16,22 +29,32 @@ f_before()
 	return
 }
 
-f_test_regular_file()
+f_test_file()
 {
 	# Test that file was copied
-	if [[ ! -f "${dest}/$1" ]]; then
+	if [[ ! -f "${dest}/${destfile}" ]]; then
 		echo "Error: $1: no such file or directory"
 		exit 1
 	fi
 	# Test for correctness of content
-	if [[ `diff "${src}/files/$1" "${dest}/$1"` ]]; then
+	if [[ `diff "${src}/$3/$1" "${dest}/${destfile}"` ]]; then
 		echo "Error: test.txt does not contain the expected content."
 		echo "Expected:"
-		cat "${src}/files/$1"
+		cat "${src}/$3/$1"
 		echo "Found:"
-		cat "${dest}/$1"
+		cat "${dest}/${destfile}"
 		exit 1
 	fi
+}
+
+f_test_regular_file() {
+	destfile="${2:-$1}"
+	f_test_file "$1" "${destfile}" "files"
+}
+
+f_test_include_file() {
+	destfile="${2:-$1}"
+	f_test_file "$1" "${destfile}" "include"
 }
 
 f_after()
@@ -46,6 +69,12 @@ f_after()
 	f_test_regular_file "test.txt"
 	f_test_regular_file "filename with spaces.txt"
 	f_test_regular_file ".hiddenfile"
+	f_test_include_file "extrafile.txt"
+	f_test_include_file "extra file with spaces.txt"
+	f_test_include_file "extra file with spaces.txt" "customname.txt"
+	f_test_include_file "extra file with spaces.txt" "nested/customname.txt"
+	f_test_include_file "extra file with spaces.txt" "nested/custom name with spaces.txt"
+	f_test_include_file "extra file with spaces.txt" "nested folder with spaces/custom name with spaces.txt"
 	
 	# Note that this function must not be completely empty,
 	# hence the `return` statement.
